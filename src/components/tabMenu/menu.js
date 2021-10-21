76894b8db9b8055fa21318e36b21242ba696ab98
jekkiboi <hollymayer4@gmail.com>
2021-10-20 23:33:02 -0700

    img

--- src/components/tabMenu/menu.js
+++ src/components/tabMenu/menu.js
@@ -1,47 +1,41 @@
-import iconwayfarer from '/images/iconwayfarer.png'
-import C9icon from '/images/C9icon.png'
-import Lizzieicon from '/images/Lizzieicon.png'
-import diamond from '/images/diamond.png'
-import oftheearth from '/images/oftheearth.png'
+
 
 const Menu = [
 {
     id: 1,
-    image: iconwayfarer,
+    image: "images/iconwayfarer.png",
     name: "Project Wayfarer",
     link: "/wayfarer",
     category: "Web",
 },
 {
     id: 2,
-    image: C9icon,
-    name: 'Claude 9',
+    image: "images/C9icon.png",
+    name: "Claude 9",
     link: "/claude9",
     category: "Graphic",
 },
 {
     id: 3,
-    image: Lizzieicon,
+    image: "images/Lizzieicon.png",
     name: "Lizzie",
     link: "/lizzie",
     category: "Web",
 },
 {
     id: 4,
-    image: diamond,
+    image: "images/diamond.png",
     name: "EnGayged",
     link: "/engayged",
     category: "Web",
 },
 {
     id: 5,
-    image: oftheearth,
+    image: "images/oftheearth.png",
     name: "Of the Earth",
     link: "/oftheearth",
     category: "Graphic",
 },
-
-
 
 ]
 export default Menu
